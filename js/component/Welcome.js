de42dfa8aa4e8a2942cb129bc47b07e120f4a5ab
ln714360720 <ln714360720@163.com>
2020-10-16 18:56:30 +0800

    添加导航条,先显示欢迎页

--- js/component/Welcome.js
+++ js/component/Welcome.js
@@ -1,6 +1,20 @@
 import React,{Component} from 'react'
 import {View,StyleSheet,Text} from 'react-native'
 export default class Welcome extends Component{
+    constructor(props){
+        super(props);
+
+    }
+   componentDidMount(): void {
+        this.timer=setInterval(()=>{
+            this.props.navigation.reset({index:0,routes:[{name:'Home'}]})
+            console.log(this.props.navigation.reset('Home'))
+        },4000)
+   }
+    componentWillUnmount(): void {
+        this.timer&& clearInterval(this.timer)
+    }
+
     render() {
         return (
             <View><Text>欢迎页面</Text></View>
